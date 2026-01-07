d3394d9ec581a2d5e79e849f178a8182217cbe5d
ghost-mann <iamaob8@protonmail.com>
2026-01-07 18:37:38 +0300

    feat: added styles.css

--- login.php
+++ login.php
@@ -6,13 +6,28 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
 <h1>Welcome! Kindly Enter Your Details </h1>
-Name:<input id="name" type="text">
-Email:<input id="email" type="email">
-Password:<input id="password" type="password">
-<button id="signup" type="button">Sign Up</button>
-<button id="signup" type="submit">Submit</button>
+
+<div class="container">
+    <form class="form-group">
+        <div class="form-group">
+            Name:<input id="name" type="text">
+        </div>
+        <div class="form-group">
+            Email:<input id="email" type="email">
+        </div>
+        <div class="form-group">
+            Password:<input id="password" type="password">
+        </div>
+        <div class="form-group">
+            <button class="button" id="signup" type="button">Sign Up</button>
+            <button class="second-button" id="signup" type="submit">Submit</button>
+        </div>
+    </form>
+</div>
 </body>
 </html>
