6aaa91196880657825d0b2f714fb6d07111046d0
ghost-mann <iamaob8@protonmail.com>
2026-01-07 16:00:26 +0300

    feat: created a simple login page in login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<h1>Welcome! Kindly Enter Your Details </h1>
+Name:<input id="name" type="text">
+Email:<input id="email" type="email">
+Password:<input id="password" type="password">
+<button id="signup" type="button">Sign Up</button>
+<button id="signup" type="submit">Submit</button>
+</body>
+</html>
